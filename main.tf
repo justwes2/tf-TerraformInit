c6c7517ba50563520f321c4086e8df9762ed9160
wes <wes.coffay@gmail.com>
2020-02-06 23:21:25 -0500

    added remote state and bucket resource

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+    backend "s3" {
+        bucket = "sts-nonprod-terraform"
+        key = "TerraformInit"
+        region = "us-east-1"
+    }
+}
+resource "random_id" "bucket_name" {
+  byte_length = 4
+  prefix = "bucket-"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = random_id.bucket_name.hex
+  acl    = "private"
+  force_destroy = true
+}
